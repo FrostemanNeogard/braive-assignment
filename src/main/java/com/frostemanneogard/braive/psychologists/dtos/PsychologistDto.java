152a24785b2c98b89933224817d0b0cd57c1d298
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-26 23:21:58 +0100

    add patientIds field

--- src/main/java/com/frostemanneogard/braive/psychologists/dtos/PsychologistDto.java
+++ src/main/java/com/frostemanneogard/braive/psychologists/dtos/PsychologistDto.java
@@ -1,9 +1,12 @@
 package com.frostemanneogard.braive.psychologists.dtos;
 
+import com.frostemanneogard.braive.base.BaseEntity;
 import com.frostemanneogard.braive.organizations.dtos.OrganizationDto;
+import com.frostemanneogard.braive.patients.dtos.PatientDto;
 import com.frostemanneogard.braive.psychologists.Psychologist;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 public record PsychologistDto(
@@ -11,15 +14,21 @@ public record PsychologistDto(
         String name,
         String nationalIdNum,
         LocalDate dateOfBirth,
-        OrganizationDto organization) {
+        OrganizationDto organization,
+        List<UUID> patientIds) {
 
     public static PsychologistDto fromEntity(Psychologist psychologist) {
+        if (psychologist == null) {
+            return null;
+        }
+
         return new PsychologistDto(
                 psychologist.getId(),
                 psychologist.getName(),
                 psychologist.getNationalIdNum(),
                 psychologist.getDateOfBirth(),
-                OrganizationDto.fromEntity(psychologist.getOrganization())
+                OrganizationDto.fromEntity(psychologist.getOrganization()),
+                psychologist.getPatients().stream().map((BaseEntity::getId)).toList()
         );
     }
 
