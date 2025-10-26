8b5752bf753004312484ed42bdb732568dce5b9d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-26 21:54:29 +0100

    remove old organization relation

--- src/main/java/com/frostemanneogard/braive/patients/Patient.java
+++ src/main/java/com/frostemanneogard/braive/patients/Patient.java
@@ -5,15 +5,10 @@ import com.frostemanneogard.braive.organizations.Organization;
 import jakarta.persistence.*;
 import lombok.Getter;
 
-import java.util.UUID;
-
 @Entity
 @Table(name = "patients")
 @Getter
 public class Patient extends Person {
-
-    @Column
-    private UUID organizationId;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "organization_id")
