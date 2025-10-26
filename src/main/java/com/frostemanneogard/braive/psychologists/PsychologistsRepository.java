88ecb3a99cfd32e709c4e02c28cf7f9183b77575
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-26 22:41:09 +0100

    define method with query to fetch psychologists based on patients assigned in ascending order

--- src/main/java/com/frostemanneogard/braive/psychologists/PsychologistsRepository.java
+++ src/main/java/com/frostemanneogard/braive/psychologists/PsychologistsRepository.java
@@ -1,8 +1,20 @@
 package com.frostemanneogard.braive.psychologists;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PsychologistsRepository extends ListCrudRepository<Psychologist, UUID> {
+
+    @Query("""
+        SELECT p 
+        FROM Psychologist p
+        LEFT JOIN p.patients pat
+        GROUP BY p.id
+        ORDER BY COUNT(pat) ASC
+    """)
+    List<Psychologist> findWorkloadOrderedAscending();
+
 }
