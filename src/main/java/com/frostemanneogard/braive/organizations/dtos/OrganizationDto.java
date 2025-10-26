77d92a954ca5bcadb120e1255a10fb1804dfb83f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-26 23:22:03 +0100

    add null check

--- src/main/java/com/frostemanneogard/braive/organizations/dtos/OrganizationDto.java
+++ src/main/java/com/frostemanneogard/braive/organizations/dtos/OrganizationDto.java
@@ -7,6 +7,10 @@ import java.util.UUID;
 public record OrganizationDto(UUID id, String name) {
 
     public static OrganizationDto fromEntity(Organization org) {
+        if (org == null) {
+            return null;
+        }
+
         return new OrganizationDto(org.getId(), org.getName());
     }
 
