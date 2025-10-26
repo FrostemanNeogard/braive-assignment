cc645427e4d13a0ed78a56136d60e7d96e1700a9
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2025-10-26 22:01:28 +0100

    replace constructor with lombok annotation

--- src/main/java/com/frostemanneogard/braive/patients/PatientsService.java
+++ src/main/java/com/frostemanneogard/braive/patients/PatientsService.java
@@ -1,18 +1,16 @@
 package com.frostemanneogard.braive.patients;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Set;
 
 @Service
+@RequiredArgsConstructor
 public class PatientsService {
 
     private final PatientsRepository patientsRepository;
-
-    public PatientsService(PatientsRepository patientsRepository) {
-        this.patientsRepository = patientsRepository;
-    }
 
     public List<Patient> getAllPatients() {
         return this.patientsRepository.findAll();
